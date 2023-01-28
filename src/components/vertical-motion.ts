3a930e2d72b82cabf6344bab7922924b61f416f4
Ahmed Osama <aosama@sahlpay.app>
2023-01-28 13:00:12 +0200

    add VerticalMotion component

---
+++ src/components/vertical-motion.ts
@@ -0,0 +1,52 @@
+import {
+    Component
+} from "../utils/ecs/component";
+import {
+    VerticalMotionState
+} from "../utils/enums/vertical-motion-state";
+
+
+export class VerticalMotion extends Component {
+
+    private _velocity = 0;
+    private _state = VerticalMotionState.Falling;
+    private _timeWhenStateUpdated = Date.now();
+    private _velocityWhenStateUpdated = 0;
+    
+    constructor(
+        public maxVelocity: number,
+        public fall: (elapsed: number, initialValue: number, amountOfChange: number, duration: number) => number,
+    ) {
+        super();
+    }
+
+    get velocity(): number {
+        return this._velocity;
+    }
+
+    updateVelocity() {
+        if(this._state == VerticalMotionState.Falling) {
+            this._velocity = this.fall(this.getTimeElapsedAtCurrentState(), this._velocityWhenStateUpdated, this.maxVelocity-this._velocityWhenStateUpdated, 1000);
+        }
+        else if(this._state == VerticalMotionState.OnGround) {
+            this._velocity = 0;
+        }
+    }
+
+    get state(): VerticalMotionState {
+        return this._state;
+    }
+
+    set state(value: VerticalMotionState) {
+        if(this._state != value) {
+            this._state = value;
+            this._timeWhenStateUpdated = Date.now();
+            this._velocityWhenStateUpdated = this.velocity;
+        }
+    }
+
+    getTimeElapsedAtCurrentState(): number{
+        return Date.now() - this._timeWhenStateUpdated;
+    }
+}
+
